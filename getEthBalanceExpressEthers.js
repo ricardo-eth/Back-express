1154ee834ff22b7588488ea2849042cc7798fad6
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-07-12 18:56:42 +0200

    Go

--- getEthBalanceExpressEthers.js
+++ getEthBalanceExpressEthers.js
@@ -1,3 +1,4 @@
+const { ethers } = require("ethers");
 const express = require("express");
 const app = express();
 
@@ -8,7 +9,10 @@ const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
 const IP_LOOPBACK = "localhost";
 const PORT = 3000;
 
-const provider = new InfuraProvider("Rinkeby", INFURA_PROJECT_ID);
+const provider = new ethers.providers.InfuraProvider(
+  "Rinkeby",
+  INFURA_PROJECT_ID
+);
 
 app.get("/", (req, res) => {
   res.send(
@@ -20,6 +24,7 @@ app.get("/getBalance/:address", async (req, res) => {
   res.send(provider.getBalance(req.params.address));
 });
 
-app.listen(PORT, async () => {
-  console.log(`App listening at http://${IP_LOOPBACK}:${PORT}`);
+// start the server
+app.listen(PORT, IP_LOOPBACK, () => {
+  console.log(`Example app listening at http://${IP_LOOPBACK}:${PORT}`);
 });
